fdac22bbcbf616e283727aa3ec3ae782542d2894
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2021-12-08 20:20:55 -0500

    5.2

--- Vous êtes le héros/chap_01.js
+++ Vous êtes le héros/chap_01.js
@@ -1,3 +1,4 @@
+const imgText = './Vous êtes le héros/assets/image_texte.jpg'
 
 const checkbox = document.querySelector('.input' )
 let soundOrNot = true
@@ -6,7 +7,7 @@ checkbox.addEventListener('change', function(){
     soundOrNot = checkbox.checked; 
 })
 
-const son = new Audio('assets/boop.mp3');
+const son = new Audio('./Vous êtes le héros/assets/boop.mp3');
 
 let tookWatch = false;
 if (localStorage.getItem("tookWatch") != undefined) {
@@ -30,7 +31,7 @@ let chaptersObj = {
     kyle_text_mia: {
         subtitle: 'Kyle is worried',
         text: 'Its evening, and Mia does not seem to be at home. She didnt tell me she had to stay late at work. Maybe I should text her to see if everything is fine. Hey! Are you alright? Will you be home soon?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_sentence1")'
@@ -40,7 +41,7 @@ let chaptersObj = {
     kdnp_sentence1: {
         subtitle: 'Mia is not coming home',
         text: 'Unfortunately no, she wont be back really soon.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_and_kdnp")'
@@ -50,7 +51,7 @@ let chaptersObj = {
     kyle_and_kdnp: {
         subtitle: 'Kyle is shocked',
         text: 'Hold on!',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'Where is Mia?',
             action: 'goToChapter("kyle_question1")'
@@ -63,7 +64,7 @@ let chaptersObj = {
     kyle_question1: {
         subtitle: 'Where is Mia?',
         text: 'Where is Mia?!',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_answer1")'
@@ -73,7 +74,7 @@ let chaptersObj = {
     kdnp_answer1: {
         subtitle: 'The answer to Kyles question about Mia',
         text: 'Dont worry, shes ok...',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("what_kyle_wants")'
@@ -83,7 +84,7 @@ let chaptersObj = {
     kyle_question2: {
         subtitle: 'Who are you?',
         text: 'Who the hell are you?!',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_answer2")'
@@ -93,7 +94,7 @@ let chaptersObj = {
     kdnp_answer2: {
         subtitle: 'The kidnapper presents himself',
         text: 'Im just a guy who wants to play a little game with you. Its called: Give me what I want or she dies.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("what_kyle_wants")'
@@ -103,7 +104,7 @@ let chaptersObj = {
     what_kyle_wants: {
         subtitle: 'what Kyle wants',
         text: 'Alright look, I will give you what you want, but first I want to talk to her.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_refuse")'
@@ -113,7 +114,7 @@ let chaptersObj = {
     kdnp_refuse: {
         subtitle: 'Kyle cannot talk to Mia',
         text: 'Sorry, that wont be possible for now.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_threat")'
@@ -123,7 +124,7 @@ let chaptersObj = {
     kyle_threat: {
         subtitle: 'Kyle threatens to call the cops',
         text: 'You know what, im calling the cops.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_threat")'
@@ -133,7 +134,7 @@ let chaptersObj = {
     kdnp_threat: {
         subtitle: 'The kidnapper threatens kyle',
         text: 'If you call the cops, she dies.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'call the cops',
             action: 'goToChapter("kyle_calls_cops")'
@@ -146,7 +147,7 @@ let chaptersObj = {
     kyle_calls_cops: {
         subtitle: 'Kyle decides to call the cops anyway',
         text: 'Kyle decided to call the cops anyway, but the kidnapper wasnt playing games with him. As a result, Mia died.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'try again',
             action: 'goToChapter("kyle_text_mia")'
@@ -156,7 +157,7 @@ let chaptersObj = {
     kyle_collaborates: {
         subtitle: 'Kyle collaborates',
         text: 'Alright then, I wont call the cops.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_ask_kdnp")'
@@ -166,7 +167,7 @@ let chaptersObj = {
     kyle_ask_kdnp: {
         subtitle: 'Kyle asks to the kidnapper what he wants',
         text: 'So what do you want from me.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("what_kdnp_wants")'
@@ -176,8 +177,8 @@ let chaptersObj = {
     what_kdnp_wants: {
         subtitle: 'What the kidnapper wants',
         text: 'I want money.',
-        img: 'assets/image_texte.jpg',
-        video: 'assets/video2.mp4',
+        img: imgText,
+        video: './Vous êtes le héros/assets/video2.mp4',
         option: [{
 
             text: 'How much',
@@ -194,7 +195,7 @@ let chaptersObj = {
     kyle_refuse1: {
         subtitle: 'Kyle refuses to give money to the kidnapper',
         text: 'Forget it, Im not giving you any money.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_kills_mia2")'
@@ -204,7 +205,7 @@ let chaptersObj = {
     kdnp_kills_mia2: {
         subtitle: 'The kidnapper kills Mia',
         text: 'Thats a shame, I guess then that if you dont want to give me what I want, then I dont need your girlfriend anymore. Mia dies.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'try again',
             action: 'goToChapter("kyle_text_mia")'
@@ -214,7 +215,7 @@ let chaptersObj = {
     kyle_howmuch_money: {
         subtitle: 'Kyle asks how much money does the kidnapper want',
         text: 'How much do you want?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_answer_money")'
@@ -224,7 +225,7 @@ let chaptersObj = {
     kdnp_answer_money: {
         subtitle: 'How much money does he want',
         text: 'I want 80,000$.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'Accept',
             action: 'goToChapter("kyle_accepts")'
@@ -237,7 +238,7 @@ let chaptersObj = {
     kyle_accepts: {
         subtitle: 'Kyle accepts to give 80,000$ to the kidnapper',
         text: 'ok...',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_infos")'
@@ -247,7 +248,7 @@ let chaptersObj = {
     kyle_choices: {
         subtitle: 'What should Kyle take before leaving?.',
         text: 'What should I take before leaving?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'Take the watch',
             action: 'watchOrNot()'
@@ -260,7 +261,7 @@ let chaptersObj = {
     kyle_infos: {
         subtitle: 'Kyle asks where should he give the money',
         text: 'where do I give it to you?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_gives_address")'
@@ -270,7 +271,7 @@ let chaptersObj = {
     kdnp_gives_address: {
         subtitle: 'Where does Kyle should give the money',
         text: 'Heres the address: 813 Howard Street. At 22:00, dont be late because you know what will happen to your girlfriend.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_choices")'
@@ -280,7 +281,7 @@ let chaptersObj = {
     kyle_goesto_address: {
         subtitle: 'Kyle goes to the address',
         text: 'Kyle goes to the address.',
-        video: 'assets/video1.mp4',
+        video: './Vous êtes le héros/assets/video1.mp4',
         option: [{
             text: 'continue',
             action: 'goToChapter("kyles_watch")'
@@ -290,7 +291,7 @@ let chaptersObj = {
     kyles_watch: {
         subtitle: 'Kyle needs to check the time to find out if hes late, but does he have his watch?',
         text: 'I should check the time, but do I have my watch?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         localisation: 1,
         option: [{
             text: 'continue',
@@ -301,7 +302,7 @@ let chaptersObj = {
     kyle_no_watch: {
         subtitle: 'Kyle doesnt have his watch with him',
         text: 'Kyle didnt bring his watch with him. He will therefore not be able to get to the appointment on time, which will make Mia dies.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'try again',
             action: 'goToChapter("kyle_text_mia")'
@@ -311,7 +312,7 @@ let chaptersObj = {
     kyle_has_watch: {
         subtitle: 'Kyle has his watch',
         text: 'Alright, I have my watch and I still have some time. Should I try to find Mia first or should I give him the money first?',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'find Mia first',
             action: 'goToChapter("kyle_finds_mia_first")'
@@ -324,7 +325,7 @@ let chaptersObj = {
     kyle_finds_mia_first: {
         subtitle: 'Kyle wants to find Mia first',
         text: 'Kyle got lost trying to find Mia by himself. He will therefore not be able to get to the appointment on time, which will make Mia dies.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'try again',
             action: 'goToChapter("kyle_text_mia")'
@@ -334,7 +335,7 @@ let chaptersObj = {
     kyle_brings_money_first: {
         subtitle: 'Kyle wants to bring the money to the kidnapper first',
         text: 'The money is there. Now I want to see her!',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kdnp_brings_mia")'
@@ -344,7 +345,7 @@ let chaptersObj = {
     kdnp_brings_mia: {
         subtitle: 'The kidnapper arrives with Mia, takes the money, releases Mia and leaves',
         text: 'The kidnapper arrives with Mia, takes the money, releases Mia and leaves.',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'continue',
             action: 'goToChapter("kyle_mia_leave")'
@@ -354,7 +355,7 @@ let chaptersObj = {
     kyle_mia_leave: {
         subtitle: 'Kyle leaves with Mia',
         text: 'Mia leaves safely with Kyle. THE END',
-        img: 'assets/image_texte.jpg',
+        img: imgText,
         option: [{
             text: 'try again',
             action: 'goToChapter("kyle_text_mia")'
